152e136d5e5e1001cba13d5a24ea329499e6121d
Ariana nocetti <166346246+AriNocetti@users.noreply.github.com>
2025-06-18 21:38:24 -0300

    Update mocks.router.js

--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -3,9 +3,9 @@ import mocksController from "../controllers/mocks.controller.js";
 
 const router = Router();
 
-router.get("/", mocksController.index);
+// router.get("/", mocksController.index);
 router.get("/mockingpets", mocksController.mockingPets);
-router.post("/mockingusers", mocksController.mockingUsers);
+router.get("/mockingusers", mocksController.mockingUsers);
 router.post("/generateData", mocksController.generateData);
 
 export default router;
